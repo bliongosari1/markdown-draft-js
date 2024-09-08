c3f5222e1118038de3a8dcd0391ae3545cf548b4
bliongosari <95192747+bliongosari1@users.noreply.github.com>
2024-09-08 12:32:25 +1000

    Update markdown-to-draft.js

--- src/markdown-to-draft.js
+++ src/markdown-to-draft.js
@@ -269,9 +269,11 @@ function markdownToDraft(string, options = {}) {
       // If there’s nested block level items deeper than that, we need to make sure we capture this by cloning the topmost block
       // otherwise we’ll accidentally overwrite its text. (eg if there's a blockquote with 3 nested paragraphs with inline text, without this check, only the last paragraph would be reflected)
       if (item.level === 0 || item.type === 'list_item_open') {
-        block = Object.assign({
-          depth: depth
-        }, BlockTypes[itemType](item));
+        // block = Object.assign({
+        //   depth: depth
+        // }, BlockTypes[itemType](item));
+        // don't add empty blocks to the block array
+        block = BlockTypes[itemType](item);
       } else if (item.level > 0 && blocks[blocks.length - 1].text) {
         block = Object.assign({}, blocks[blocks.length - 1]);
       }
